135971efaf69b21d1761c393a335fc9883525dad
Nitin ram <89689699+nitinram29@users.noreply.github.com>
2022-09-03 01:36:44 +0530

    Time: 1705 ms (5.00%), Space: 118.4 MB (6.87%) - LeetHub

--- 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.java
+++ 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.java
@@ -1,8 +1,8 @@
-class Solution {
+class Solution {    
     public int secondsToRemoveOccurrences(String s) {
         int ans=0;
-        while(!s.equals(s.replaceAll("01","10"))){
-            s = s.replaceAll("01","10");
+        while(!s.equals(s=s.replaceAll("01","10"))){
+            //s = s.replaceAll("01","10");
             ans++;         
         }
         return ans;        
