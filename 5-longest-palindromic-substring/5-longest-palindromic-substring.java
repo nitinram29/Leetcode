c33180435c11cf555b70eb2e24e8f7afc6ba4014
Nitin ram <89689699+nitinram29@users.noreply.github.com>
2022-09-05 01:50:57 +0530

    Time: 12 ms (97.37%), Space: 42.7 MB (77.48%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,27 +1,30 @@
 class Solution {
-    public String helper(char[] arr,int i,int j){
+    public int helper(char[] arr,int i,int j){
         String ans="";
         while(i>=0 && j<arr.length){
             if(arr[i]==arr[j]){
-                if(i!=j)
-                ans = ""+arr[i]+ans+arr[j];
-                else ans+=arr[i];
-            }else return ans;
-            i--;
-            j++;
+                i--;
+                j++;
+            }else break;
         }
-        return ans;        
+        return j-i-1;        
     }
     public String longestPalindrome(String s) {
         if(s.length()==1) return s;
-        String ans="";
+        //String ans="";
+        int st=0;
+        int e=0;
         char[] arr = s.toCharArray();
-        for(int i=0;i<arr.length-1;i++){
-            String x = helper(arr,i,i);
-            String y = helper(arr,i,i+1);
-            String tmp = x.length()>y.length()?x:y;
-            ans = tmp.length()>ans.length()?tmp:ans;
+        for(int i=0;i<arr.length;i++){
+            int x = helper(arr,i,i);
+            int y = helper(arr,i,i+1);
+            
+            x=x>y?x:y;
+            if(x>e-st){
+                st = i-((x-1)/2);
+                e = i+(x/2);
+            }
         }
-        return ans;        
+        return s.substring(st,e+1);        
     }
 }
