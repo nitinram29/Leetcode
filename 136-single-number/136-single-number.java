d74fd72adf22a0431edb2c848cc02c711ad4678a
Nitin ram <89689699+nitinram29@users.noreply.github.com>
2022-09-05 00:53:44 +0530

    Time: 2 ms (65.15%), Space: 50.3 MB (70.65%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,13 +1,12 @@
 class Solution {
-    public int singleNumber(int[] nums) {
+    public int singleNumber(int[] nums) {        
         if(nums.length==1) return nums[0];
-        Map<Integer,Integer> map = new HashMap<>();
         
+        // as XOR of 2 same numbers is 0
+        // eg. 2,1,2 so 2^2 results in 0 so the ans will contain only 1
+        int ans=0;
         for(int i:nums)
-            map.put(i,map.getOrDefault(i,0)+1);
-        for(Map.Entry<Integer,Integer> i: map.entrySet())
-            if(i.getValue()==1) return i.getKey();
-        
-        return 0;        
+            ans^=i;
+        return ans; 
     }
 }
