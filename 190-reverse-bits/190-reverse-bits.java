5ac26563c173dc4753d0fbeb433e3fad7245106a
Nitin ram <89689699+nitinram29@users.noreply.github.com>
2022-09-05 00:43:11 +0530

    Time: 2 ms (28.47%), Space: 42.3 MB (62.34%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.java
+++ 190-reverse-bits/190-reverse-bits.java
@@ -1,5 +1,6 @@
 public class Solution {
     // you need treat n as an unsigned value
+    // Easy but have to take a look as this may slip from mind
     public int reverseBits(int n) {
         int ans=0;
         for(int i=0;i<32;i++){
