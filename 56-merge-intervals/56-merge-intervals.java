e89db65424523e77ffc3dceee859db2ddbd9a801
Nitin ram <89689699+nitinram29@users.noreply.github.com>
2022-09-05 15:03:07 +0530

    Time: 18 ms (24.07%), Space: 54.7 MB (80.33%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,12 +1,17 @@
 class Solution {
-    // Easy solution
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
-        List<int[]> list=  new ArrayList<>();
+        
+        Arrays.sort(intervals,(l1,l2)->Integer.compare(l1[0],l2[0]));
+        List<int[]> list = new ArrayList<>();
         for(int[] i:intervals){
-            if(list.size()==0 || list.get(list.size()-1)[1]<i[0]) list.add(i);
-            else list.get(list.size()-1)[1] = Math.max(list.get(list.size()-1)[1],i[1]);   
+            
+            if(list.isEmpty() || list.get(list.size()-1)[1]<i[0]){
+                list.add(i);
+            }
+            else{
+                list.get(list.size()-1)[1] = Math.max(list.get(list.size()-1)[1],i[1]);
+            }
         }
-        return list.toArray(new int[list.size()][2]);
+        return list.toArray(new int[list.size()][]);
     }
 }
