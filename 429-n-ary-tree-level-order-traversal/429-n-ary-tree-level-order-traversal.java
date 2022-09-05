c32ede0e193a7556f999e7e7729764e4c109fa4c
Nitin ram <89689699+nitinram29@users.noreply.github.com>
2022-09-06 00:02:55 +0530

    Time: 5 ms (43.54%), Space: 47.6 MB (30.43%) - LeetHub

--- 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.java
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.java
@@ -1,22 +1,3 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public List<Node> children;
-
-    public Node() {}
-
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, List<Node> _children) {
-        val = _val;
-        children = _children;
-    }
-};
-*/
-
 class Solution {
     public List<List<Integer>> levelOrder(Node root) {
         Queue<Node> q = new LinkedList<>();
