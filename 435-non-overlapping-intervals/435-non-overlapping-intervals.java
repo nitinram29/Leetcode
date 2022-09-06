0a617227e4e95b5b5298372d3fd600f0129de50c
Nitin ram <89689699+nitinram29@users.noreply.github.com>
2022-09-06 14:18:09 +0530

    Time: 99 ms (48.97%), Space: 98.6 MB (96.50%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.java
@@ -3,9 +3,7 @@ class Solution {
         Arrays.sort(intervals,(a,b)->Integer.compare(a[1],b[1]));
         int ans=0;
         for(int i=0,j=1;j<intervals.length;j++){
-            int[] tmp = intervals[i];
-            int[] tmp1 = intervals[j];
-            if(tmp1[0]<tmp[1]){
+            if(intervals[j][0]<intervals[i][1]){
                 ans++;
             }else {
                 i=j;
