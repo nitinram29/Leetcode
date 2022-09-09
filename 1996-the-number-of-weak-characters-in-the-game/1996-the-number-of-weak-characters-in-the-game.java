eeec6308dd53c4dae8838de1fdaa1a1c8029f892
Nitin ram <89689699+nitinram29@users.noreply.github.com>
2022-09-10 02:02:26 +0530

    Time: 195 ms (36.75%), Space: 130.1 MB (68.58%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.java
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.java
@@ -4,13 +4,11 @@ class Solution {
         int n = properties.length;
         int count = 0;
         Arrays.sort(properties, (a, b) -> (b[0] == a[0]) ? (b[1] - a[1]) : a[0] - b[0]);
- 
         int max = 0;
         for (int i = n-1;i>=0;i--) {
             if (properties[i][1] < max) {
                 count++;
-            }
-            max = Math.max(max, properties[i][1]);
+            }else max =  properties[i][1];
         }
         return count;
     }
