4f881f296ded84fa3b01894526d1bdb0cfdff31e
Nitin ram <89689699+nitinram29@users.noreply.github.com>
2022-09-09 13:14:43 +0530

    Time: 133 ms (13.89%), Space: 44.5 MB (43.40%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -0,0 +1,23 @@
+class Solution {
+    int ans = 0;
+    public int helper(int[] nums,int ind,int[] dp){  
+        if(dp[ind]!=-1) return dp[ind];
+        int ans=0;
+        for(int i=ind+1;i<nums.length;i++){
+            if(nums[i]>nums[ind]){
+                ans= Math.max(ans,(helper(nums,i,dp)+1));
+            }
+        }
+        //ans = Math.max(ans,len);
+        return dp[ind] = ans;
+    }
+    
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp,-1);
+        for(int i=0;i<nums.length;i++){
+            ans = Math.max(ans,helper(nums,i,dp));
+        }
+        return ans+1;   
+    }
+}
